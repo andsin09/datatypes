cc8689e11deb6f26abc2500ac730450af593c852
andsin09 <161988024+andsin09@users.noreply.github.com>
2024-09-10 09:01:59 -0400

    Add files via upload

---
+++ datatypes.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+int a;
+float b;
+char c;
+bool d; 
+int main()
+{
+    std::cout << "Hello World" << std::endl;
+
+    int a = 1;
+    float b = 32451234311234;
+    char c = 'c';
+    bool d = false;
+    std::string e = "hello";
+    std::cout << a << " " << b << " " << c << " " << d << " " << e << std::endl;
+}
